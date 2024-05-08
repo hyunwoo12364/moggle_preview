ef1984ea1030099687a1e8c3c8f4c8cec6692818
hyunwoo12364 <sdmyhw@gmail.com>
2024-05-08 14:05:39 +0900

    Delete moggle_preview.py

--- moggle_preview.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-train = pd.read_csv('train.csv')
-train
-test = pd.read_csv('test.csv')
-submission = pd.read_csv('sample_submission.csv')
-submission.head()
-submission.to_csv('submission.csv', index=False)
